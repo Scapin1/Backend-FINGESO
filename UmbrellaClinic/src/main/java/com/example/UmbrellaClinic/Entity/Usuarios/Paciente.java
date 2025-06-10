65378f68ef9b231b7f884b2d603f7f8320c9a4c1
Lucciano Camilo Scapini yañez <lucciano.scapini@usach.cl>
2025-06-10 01:17:27 -0400

    erros solve

--- UmbrellaClinic/src/main/java/com/example/UmbrellaClinic/Entity/Usuarios/Paciente.java
+++ UmbrellaClinic/src/main/java/com/example/UmbrellaClinic/Entity/Usuarios/Paciente.java
@@ -1,6 +1,9 @@
 package com.example.UmbrellaClinic.Entity.Usuarios;
 
 import com.example.UmbrellaClinic.Entity.Cita;
+import com.example.UmbrellaClinic.Entity.Examen;
+import com.example.UmbrellaClinic.Entity.HistorialMedico;
+import com.example.UmbrellaClinic.Entity.Receta;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -30,7 +33,8 @@ public class Paciente extends Usuario {
 
     @OneToMany(mappedBy = "paciente")
     @JsonIgnore
-    private List<Cita> citasList;
+    private List<Examen> examenList;
+
     @OneToMany(mappedBy = "paciente")
     @JsonIgnore
     private List<Receta> recetas; // Una lista de recetas de este paciente
