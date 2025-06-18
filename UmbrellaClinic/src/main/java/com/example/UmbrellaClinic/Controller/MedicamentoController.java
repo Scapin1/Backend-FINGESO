adde246d076f4a1d80085846f5e305f04930f3fb
Matias Ramirez <matias.ramirez.e@usach.cl>
2025-06-18 17:02:39 -0400

    comentando medicamento y receta ambos controller

--- UmbrellaClinic/src/main/java/com/example/UmbrellaClinic/Controller/MedicamentoController.java
+++ UmbrellaClinic/src/main/java/com/example/UmbrellaClinic/Controller/MedicamentoController.java
@@ -3,8 +3,6 @@ package com.example.UmbrellaClinic.Controller;
 import com.example.UmbrellaClinic.Entity.Medicamento;
 import com.example.UmbrellaClinic.Service.Impl.MedicamentoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,38 +10,44 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/medicamento")
 public class MedicamentoController {
+    // se inyectan las dependencias de su capa de servicios
+    // MedicamentoServiceImpl logica de negocios
     @Autowired
     private MedicamentoServiceImpl medicamentoService;
-
+    // endpoint tipo get para retornar los medicamentos llamando a medicamentoService.findAll()
     @GetMapping("/getMedicamentos")
     public List<Medicamento> getMedicamentos() {
         return medicamentoService.findAll();
     }
-
+    // endpoint tipo get para retornar un medicamento por su id
     @GetMapping("/getMedicamento/{id}")
     public Medicamento getMedicamento(@PathVariable Long id) {
         return medicamentoService.getById(id);
     }
+    // endpoint tipo get para retornar un medicamento po su nombre Comercial
     @GetMapping("/getMedicamentoPorNombre/{nombre}")
     public Medicamento getMedicamentoPorNombre(@PathVariable String nombre) {
         return medicamentoService.getByNombreComercial(nombre);
     }
+    // endpoint tipo post para crear un medicamento
     @PostMapping("/crearMedicamento")
     public void crearMedicamento(@RequestBody Medicamento medicamento) {
         medicamentoService.save(medicamento);
     }
-
+    // endpoint tipo delete para eliminar un medicamento segun su id
     @DeleteMapping("/eliminarMedicamento/{id}")
     public void eliminarMedicamento(@PathVariable Long id) {
         medicamentoService.deleteById(id);
     }
 
+    //estos ultimos dos deverian ser PUT no post
+
+    // endpoint del tipo post para reabastecer un medicamentos a partir de su nombre comercial y una cantidad de medicamento que llega a bodega
     @PostMapping("/reabastecerMedicamentos/{nombreComercial}/{cantidad}")
     public void reabastecerMedicamentos(@PathVariable String nombreComercial,@PathVariable int tipo,@PathVariable int cantidad) {
         medicamentoService.reabastecerMedicamentos(nombreComercial, cantidad);
     }
-    //tengo que hacer un metodo para que el quimico entregue un medicamento y de
-    // actualicen ambos stock de un medicamento que aparezca en la receta
+    // endpoint tipo Post para entregar un medicamento/s a un paciente
     @PostMapping("/entregarMedicamentos/{nombreComercial}/{cantidad}")
     public  void entregarMedicamentos(@PathVariable String nombreComercial,@PathVariable int cantidad) {
         medicamentoService.entregarMedicamentos(nombreComercial,cantidad);
--- UmbrellaClinic/src/main/java/com/example/UmbrellaClinic/Controller/RecetaController.java
+++ UmbrellaClinic/src/main/java/com/example/UmbrellaClinic/Controller/RecetaController.java
@@ -17,28 +17,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/receta")
 public class RecetaController {
+    // inyectar dependencias para conectar con la logica de negocios
     @Autowired
     private RecetaServiceImpl recetaService;
-    @Autowired
-    private MedicamentoService medicamentoService;
-
+    // endpoint tipo get para retornar todas las recetas
+    // no se si se llegue a usar
     @GetMapping("/getRecetas")
     public List<Receta> getRecetas() {
         return recetaService.findAll();
     }
+    // endpoint tipo get para retornar una receta segun su id
     @GetMapping("/getReceta/{id}")
     public Receta getReceta(@PathVariable Long id) {
         return recetaService.getById(id);
     }
-
+    // endpoint tipo post para crear una receta nueva
     @PostMapping("/crearReceta")
     public Receta crearReceta(@RequestBody Receta receta) {
         return recetaService.createReceta(receta);
     }
-
+    // endpoint tipo delete para eliminar una receta por su id
     @DeleteMapping("/eliminarReceta/{id}")
     public void eliminarReceta(@PathVariable Long id) {
     }
+    // endpoint tipo get para retornar recetas por el rut del paciente asociado
     @GetMapping("/porPacienteRut/{rutPaciente}") // Cambiado el nombre del endpoint para mayor claridad
     public ResponseEntity<List<Receta>> getRecetasByRutPaciente(@PathVariable String rutPaciente) {
         List<Receta> recetas = recetaService.findByPacienteRut(rutPaciente);
@@ -48,6 +50,7 @@ public class RecetaController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+    // endpoint tipo put para entregar la receta a un paciente segun el id de la receta y actualizar el stock
     @PutMapping("/entregarReceta/{id_receta}")
     public void entregarReceta(@PathVariable Long id_receta){
         recetaService.entregarReceta(id_receta);
