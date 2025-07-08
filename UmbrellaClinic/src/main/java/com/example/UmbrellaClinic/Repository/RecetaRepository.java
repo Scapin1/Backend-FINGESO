3f3f529666f3bc4489690ac88da1912286d86095
Matias Ramirez <matias.ramirez.e@usach.cl>
2025-07-07 23:20:38 -0400

    cambios

--- UmbrellaClinic/src/main/java/com/example/UmbrellaClinic/Repository/RecetaRepository.java
+++ UmbrellaClinic/src/main/java/com/example/UmbrellaClinic/Repository/RecetaRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface RecetaRepository extends JpaRepository<Receta, Long> {
-    List<Receta> findByPaciente_Rut(String rut); // Nota la sintaxis: 'paciente_' seguido del atributo 'Rut'
+    List<Receta> findByPaciente_Rut(String rut);
 }
